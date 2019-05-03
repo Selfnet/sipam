ae27ea7ee482cf0aff5875dc8d2f33aa6b3149ab
Cellebyte <marcel@fest4.eu>
2019-05-03 17:28:24 +0200

    Fixed migrations

--- sipam/migrations/0002_auto_20190409_2142.py
+++ sipam/migrations/0002_auto_20190409_2142.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='prefix',
             name='subdomain',
-            field=sipam.utilities.fields.URLField(blank=True, null=True),
+            field=sipam.utilities.fields.FQDNField(blank=True, null=True),
         ),
     ]
--- sipam/utilities/fields.py
+++ sipam/utilities/fields.py
@@ -1,55 +1,26 @@
 import re
+from fqdn import FQDN
 from django.db.models import CharField
-from django.core.validators import RegexValidator, deconstructible, ValidationError, _
+from django.core.validators import deconstructible, ValidationError, _
+from django import forms
 
 @deconstructible
-class URLValidator(RegexValidator):
+class FQDNValidator:
     message = _('Enter a valid FQDN.')
+    code = 'invalid'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def __call__(self, value):
-
-        # Then check full URL
-        try:
-            super().__call__(value)
-        except ValidationError as e:
-            # Trivial case failed. Try for possible IDN domain
-            if value:
-                try:
-                    scheme, netloc, path, query, fragment = urlsplit(value)
-                except ValueError:  # for example, "Invalid IPv6 URL"
-                    raise ValidationError(self.message, code=self.code)
-                try:
-                    netloc = netloc.encode('idna').decode('ascii')  # IDN -> ACE
-                except UnicodeError:  # invalid domain part
-                    raise e
-                url = urlunsplit((scheme, netloc, path, query, fragment))
-                super().__call__(url)
-            else:
-                raise
-        else:
-            # Now verify IPv6 in the netloc part
-            host_match = re.search(r'^\[(.+)\](?::\d{2,5})?$', urlsplit(value).netloc)
-            if host_match:
-                potential_ip = host_match.groups()[0]
-                try:
-                    validate_ipv6_address(potential_ip)
-                except ValidationError:
-                    raise ValidationError(self.message, code=self.code)
-
-        # The maximum length of a full host name is 253 characters per RFC 1034
-        # section 3.1. It's defined to be 255 bytes or less, but this includes
-        # one byte for the length of the name and one byte for the trailing dot
-        # that's used to indicate absolute names in DNS.
-        if len(urlsplit(value).netloc) > 253:
+        fqdn = FQDN(value)
+        if not fqdn.is_valid:
             raise ValidationError(self.message, code=self.code)
 
 
-class URLField(CharField):
-    default_validators = [validators.URLValidator()]
-    description = _("URL")
+class FQDNField(CharField):
+    default_validators = [FQDNValidator()]
+    description = _("FQDN")
 
     def __init__(self, verbose_name=None, name=None, **kwargs):
         kwargs.setdefault('max_length', 200)
