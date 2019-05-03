7f206a55bd6be7ad0a4c3e07305b0ef57b077c72
Cellebyte <marcel.fest@live.de>
2019-05-03 12:01:05 +0200

    Added fqdn for validation

--- sipam/utilities/fields.py
+++ sipam/utilities/fields.py
@@ -1,7 +1,70 @@
-from django.db.models import URLField as DefaultURLField
-from django.core.validators import URLValidator as DefaultURLValidator
+import re
+from django.db.models import CharField
+from django.core.validators import RegexValidator, deconstructible, ValidationError, _
+
+@deconstructible
+class URLValidator(RegexValidator):
+    message = _('Enter a valid FQDN.')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __call__(self, value):
+
+        # Then check full URL
+        try:
+            super().__call__(value)
+        except ValidationError as e:
+            # Trivial case failed. Try for possible IDN domain
+            if value:
+                try:
+                    scheme, netloc, path, query, fragment = urlsplit(value)
+                except ValueError:  # for example, "Invalid IPv6 URL"
+                    raise ValidationError(self.message, code=self.code)
+                try:
+                    netloc = netloc.encode('idna').decode('ascii')  # IDN -> ACE
+                except UnicodeError:  # invalid domain part
+                    raise e
+                url = urlunsplit((scheme, netloc, path, query, fragment))
+                super().__call__(url)
+            else:
+                raise
+        else:
+            # Now verify IPv6 in the netloc part
+            host_match = re.search(r'^\[(.+)\](?::\d{2,5})?$', urlsplit(value).netloc)
+            if host_match:
+                potential_ip = host_match.groups()[0]
+                try:
+                    validate_ipv6_address(potential_ip)
+                except ValidationError:
+                    raise ValidationError(self.message, code=self.code)
+
+        # The maximum length of a full host name is 253 characters per RFC 1034
+        # section 3.1. It's defined to be 255 bytes or less, but this includes
+        # one byte for the length of the name and one byte for the trailing dot
+        # that's used to indicate absolute names in DNS.
+        if len(urlsplit(value).netloc) > 253:
+            raise ValidationError(self.message, code=self.code)
 
 
-class URLField(DefaultURLField):
-    DefaultURLField.default_validators = [DefaultURLValidator(
-        schemes=None, message="Enter a valid FQDN")]
+class URLField(CharField):
+    default_validators = [validators.URLValidator()]
+    description = _("URL")
+
+    def __init__(self, verbose_name=None, name=None, **kwargs):
+        kwargs.setdefault('max_length', 200)
+        super().__init__(verbose_name, name, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        if kwargs.get("max_length") == 200:
+            del kwargs['max_length']
+        return name, path, args, kwargs
+
+    def formfield(self, **kwargs):
+        # As with CharField, this will cause URL validation to be performed
+        # twice.
+        return super().formfield(**{
+            'form_class': forms.URLField,
+            **kwargs,
+        })
