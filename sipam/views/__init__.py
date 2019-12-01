5222e4e659187f3b9325b8593cfa9f4fa35e1c27
Thore <thore@kruess.xyz>
2019-12-02 00:58:29 +0100

    Implement search on cidr

--- sipam/views/__init__.py
+++ sipam/views/__init__.py
@@ -1,3 +1,3 @@
-from .pool import PoolViewSet
 from .cidr import CIDRViewSet
 from .label import LabelViewSet
+from .pool import PoolViewSet
--- sipam/views/cidr.py
+++ sipam/views/cidr.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -12,29 +16,52 @@ class CIDRViewSet(ModelViewSet):
     """
     queryset = CIDR.objects.all()
     serializer_class = CIDRSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['cidr', 'fqdn', 'description', 'labels__value']
 
+    @method_decorator(cache_page(60))
     def list(self, request):
         """
             Get the Root Prefixes with their children
         """
-        root = []
-        for cidr in CIDR.objects.all():
-            if not cidr.supercidr:
-                root.append(cidr)
 
+        # If a search term is available apply it to the queryset
+        if self.request.query_params.get('search', None):
+            queryset = self.filter_queryset(self.queryset)
+
+        # No filter, so prune the dataset to the root networks
+        else:
+            queryset = [cidr for cidr in self.queryset if not cidr.supercidr]
+
+        # Only to this, if explicitly requested, increases load
         if self.request.query_params.get('full', False):
             return Response(
                 RecursiveCIDRSerializer(
-                    root,
+                    queryset,
                     many=True,
                     read_only=True,
                     context={'request': request}).data)
 
         return Response(
             CIDRSerializer(
-                root,
+                queryset,
                 many=True,
                 read_only=True,
+                context={'request': request}).data)
+
+    @method_decorator(cache_page(60))
+    def retrieve(self, request, pk=None):
+        cidr = get_object_or_404(self.queryset, pk=pk)
+
+        if self.request.query_params.get('full', False):
+            return Response(
+                RecursiveCIDRSerializer(
+                    cidr,
+                    context={'request': request}).data)
+
+        return Response(
+            self.serializer_class(
+                cidr,
                 context={'request': request}).data)
 
     @action(detail=True)
