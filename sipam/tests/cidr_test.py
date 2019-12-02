778f4b55c1f8abc1e366c0b18efee3f62bb232df
Thore <thore@kruess.xyz>
2019-12-02 01:58:44 +0100

    Improve cidr test cases

--- sipam/tests/cidr_test.py
+++ sipam/tests/cidr_test.py
@@ -2,9 +2,12 @@ from ipaddress import IPv4Network
 
 import pytest
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
 
-from sipam.utilities.error import NotEnoughSpace
 from sipam.utilities.enums import IP
+from sipam.utilities.error import NotEnoughSpace
+from sipam.views import CIDRViewSet
 
 
 @pytest.mark.usefixtures('testCIDR')
@@ -47,3 +50,56 @@ class CIDRTest(TestCase):
 
     def test_get_labels(self):
         assert isinstance(self.cidr.labelDict, dict)
+
+    def test_cidr_list_view(self):
+        factory = APIRequestFactory()
+        view = CIDRViewSet.as_view({'get': 'list'})
+
+        request = factory.get(reverse('cidr-list'))
+
+        response = view(request)
+        assert response.status_code == 200
+
+        # Also test the full subtree query parameter
+        request = factory.get(reverse('cidr-list'), {'full': True})
+        response = view(request)
+        assert response.status_code == 200
+
+    def test_cidr_detail_view(self):
+        factory = APIRequestFactory()
+        view = CIDRViewSet.as_view({'get': 'retrieve'})
+
+        request = factory.get(reverse('cidr-detail', kwargs={'pk': self.cidr.id}))
+
+        response = view(request, pk=self.cidr.id)
+        assert response.status_code == 200
+
+        # Also take a look at the response
+        res = response.data
+
+        assert res['cidr'] == str(self.cidr.cidr)
+
+        # Also test the full subtree query parameter
+        request = factory.get(reverse('cidr-detail', kwargs={'pk': self.cidr.id}), {'full': True})
+        response = view(request, pk=self.cidr.id)
+        assert response.status_code == 200
+
+    def test_search(self):
+        factory = APIRequestFactory()
+        view = CIDRViewSet.as_view({'get': 'list'})
+
+        # Search for a cidr
+        request = factory.get(reverse('cidr-list'), {'search': str(self.cidr.cidr)})
+        response = view(request)
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]['id'] == str(self.cidr.id)
+
+    def test_cidr_actions(self):
+        factory = APIRequestFactory()
+
+        for action in ['supercidr', 'subcidr', 'ips']:
+            view = CIDRViewSet.as_view({'get': action})
+            request = factory.get(reverse(f'cidr-{action}', kwargs={'pk': self.cidr.id}))
+            response = view(request, pk=self.cidr.id)
+            assert response.status_code == 200
