66fc3cf2a5580827436da94aba8c1a201a57d387
Thore <thore@kruess.xyz>
2020-11-21 15:12:39 +0100

    Bump to next django version

--- accounts/models/token.py
+++ accounts/models/token.py
@@ -10,10 +10,7 @@ class FlaggedToken(Token):
     """A custom Token adding a Read/Write Flag.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    key = models.CharField(_('Key'), max_length=40)
     write = models.BooleanField(null=False, default=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='auth_token',
-                             on_delete=models.CASCADE, verbose_name=_('User'))
     description = models.TextField(blank=True, null=True, default='')
 
     class Meta:
