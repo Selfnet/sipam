a139424b68d6b985223d6f743a962d6b323f496a
Cellebyte <marcel.fest@live.de>
2020-12-02 02:38:31 +0100

    Moved the auth module to the API definition

--- frontend/sipam/src/store/modules/Auth.ts
+++ frontend/sipam/src/store/modules/Auth.ts
@@ -1,25 +1,21 @@
 import Vue from 'vue';
-import axios from 'axios';
-import authAPI from '@/services/api/Auth';
+import SIPAM from '@/sipam';
 import { AuthState, BasicToken } from '@/types/store';
+import { TokenObtainPair, TokenRefresh, TokenVerify } from '@/types/api';
 import GenericAuth from './GenericAuth';
-
-const HEADER_PREFIX = 'Bearer';
 
 export default {
   namespaced: true,
-  prefix: HEADER_PREFIX,
   state: {
     token: GenericAuth.emptyState.token,
     loggedIn: GenericAuth.emptyState.loggedIn,
     username: GenericAuth.emptyState.username,
-    authHeader: GenericAuth.emptyState.authHeader,
   } as AuthState,
 
   getters: {
-    header: (state: AuthState) => state.authHeader,
+    oidcToken: (state: AuthState) => state.token.access,
     token: (state: AuthState) => state.token,
-    loggedIn: (state: AuthState) => state.loggedIn,
+    isAuthenticated: (state: AuthState) => state.loggedIn,
   },
 
   mutations: {
@@ -33,62 +29,60 @@ export default {
       Vue.set(state, 'username', username);
     },
     SAVE_TOKEN(state: AuthState, newToken: BasicToken) {
+      SIPAM.api.setSecurityData(newToken.access);
       Vue.set(state, 'token', Object.assign({}, state.token, newToken));
-      Vue.set(state, 'authHeader', `${HEADER_PREFIX} ${state.token.access}`);
-      Vue.set(axios.defaults.headers, 'Authorization', state.authHeader);
       Vue.set(state, 'loggedIn', true);
     },
     DELETE_TOKEN(state: AuthState) {
       Vue.set(state, 'token', GenericAuth.emptyState.token);
       Vue.set(state, 'loggedIn', GenericAuth.emptyState.loggedIn);
       Vue.set(state, 'username', GenericAuth.emptyState.username);
-      Vue.set(state, 'authHeader', GenericAuth.emptyState.authHeader);
-      Vue.set(axios.defaults.headers, 'Authorization', state.authHeader);
     },
   },
-
   actions: {
     async POST_LOGIN(context: { commit: any }, payload: { username: string, password: string }) {
-      const response = await authAPI.login(payload.username, payload.password);
-      if (response.status === 200) {
-        const verify = await authAPI.verifyAccess(response.data);
-        if (verify && verify.status === 200) {
-          context.commit('SAVE_USERNAME', payload.username);
-          context.commit('SAVE_TOKEN', response.data);
-        }
+      const tokenObtainPair = {
+        username: payload.username,
+        password: payload.password,
+      } as TokenObtainPair;
+      const response = await SIPAM.api.jwt.jwtCreate(tokenObtainPair);
+      const token = response.data as BasicToken;
+      const tokenVerify = {
+        token: token.access,
+      } as TokenVerify;
+      const verifyResponse = await SIPAM.api.jwt.jwtVerifyCreate(tokenVerify);
+      if (verifyResponse.data) {
+        context.commit('SAVE_USERNAME', payload.username);
+        context.commit('SAVE_TOKEN', token);
       } else {
-        console.log('Cannot Login.');
+        console.log('Cannot Verify Token.');
       }
     },
     async LOGOUT(context: { commit: any }) {
       context.commit('DELETE_TOKEN');
     },
     async VERIFY_ACCESS(context: { commit: any }, payload: { token: BasicToken }) {
-      const response = await authAPI.verifyAccess(payload.token);
-      if (response && response.status === 200) {
+      const verify = await SIPAM.api.jwt.jwtVerifyCreate(
+        { token: payload.token.access } as TokenVerify,
+      );
+      if (verify.data) {
         context.commit('UPDATE_LOGGED_IN', true);
-      } else if (response && response.status === 401) {
-        context.commit('UPDATE_LOGGED_IN', false);
-      } else {
-        console.log(response);
-      }
+      } else { context.commit('UPDATE_LOGGED_IN', false); }
+      console.log(verify);
     },
-    async REFRESH(context: { commit: any }, payload: { token: BasicToken, callback: any }) {
-      const response = await authAPI.refresh(payload.token);
-
-      // No refresh token - handle this case first
-      if (typeof response === 'undefined') {
-        context.commit('UPDATE_LOGGED_IN', false);
-      } else if (response.status === 200) {
-        context.commit('SAVE_TOKEN', response.data);
-        context.commit('UPDATE_LOGGED_IN', true);
-      } else if (response.status === 401) {
-        console.log('Refresh unauthorized');
-        context.commit('UPDATE_LOGGED_IN', false);
-      } else {
-        console.log(response);
-      }
-      return payload.callback;
-    },
+  },
+  async REFRESH(context: { commit: any }, payload: { token: BasicToken, callback: any }) {
+    const response = await SIPAM.api.jwt.jwtRefreshCreate(
+      { refresh: payload.token.refresh } as TokenRefresh,
+    );
+    if (response) {
+      context.commit('SAVE_TOKEN', response.data);
+      context.commit('UPDATE_LOGGED_IN', true);
+    } else {
+      console.log('Refresh unauthorized');
+      context.commit('UPDATE_LOGGED_IN', false);
+      console.log(response);
+    }
+    return payload.callback;
   },
 };
