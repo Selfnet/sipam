ea77313d0c5038da13ddd486e13146ff042ef0e1
Thore <thore@kruess.xyz>
2023-10-26 18:54:07 +0200

    Move to vuex-oidc v4 remove service worker entirely

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue()],
   server: {
     port: 3000,
   },
